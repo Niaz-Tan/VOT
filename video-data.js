fd9f360d666e4f8a7c97d53bbb96f55610248a78
Niaz-Tan <nuddintanim@gmail.com>
2025-11-28 02:21:54 +0600

    logo improved

--- video-data.js
+++ video-data.js
@@ -1,95 +1,95 @@
 // video-data.js - Simple data structure
 const videoData = [
-    {
-        id: "programming",
-        name: "Programming",
-        locked: true,
-        unlockMessage: "Enter programming access code",
+  {
+    id: "programming",
+    name: "Programming",
+    locked: false,
+    unlockMessage: "Enter programming access code",
+    children: [
+      {
+        id: "web-dev",
+        name: "Web Development",
+        locked: false,
+        unlockMessage: "Enter web development code",
         children: [
-            {
-                id: "web-dev",
-                name: "Web Development", 
-                locked: true,
-                unlockMessage: "Enter web development code",
-                children: [
-                    {
-                        id: "frontend",
-                        name: "Front-End",
-                        children: [
-                            {
-                                id: "html",
-                                name: "HTML",
-                                videos: [
-                                    {
-                                        id: "html-1",
-                                        title: "HTML Crash Course",
-                                        youtubeId: "UB1O30fR-EE",
-                                        description: "Learn HTML basics quickly",
-                                        duration: "1:02:35",
-                                        completed: false
-                                    }
-                                ]
-                            },
-                            {
-                                id: "css",
-                                name: "CSS", 
-                                videos: [
-                                    {
-                                        id: "css-1",
-                                        title: "CSS Tutorial",
-                                        youtubeId: "1Rs2ND1ryYc", 
-                                        description: "Complete CSS guide",
-                                        duration: "6:18:38",
-                                        completed: false
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        id: "design",
-        name: "Design",
-        children: [
-            {
-                id: "ui-design",
-                name: "UI Design",
+          {
+            id: "frontend",
+            name: "Front-End",
+            children: [
+              {
+                id: "html",
+                name: "HTML",
                 videos: [
-                    {
-                        id: "ui-1",
-                        title: "UI Design Principles",
-                        youtubeId: "9C8pxA7AecA",
-                        description: "Learn UI design basics",
-                        duration: "25:43",
-                        completed: false
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        id: "business",
-        name: "Business",
-        locked: true,
-        unlockMessage: "Enter business content code", 
-        children: [
-            {
-                id: "marketing",
-                name: "Marketing",
+                  {
+                    id: "html-1",
+                    title: "HTML Crash Course",
+                    youtubeId: "UB1O30fR-EE",
+                    description: "Learn HTML basics quickly",
+                    duration: "1:02:35",
+                    completed: false,
+                  },
+                ],
+              },
+              {
+                id: "css",
+                name: "CSS",
                 videos: [
-                    {
-                        id: "marketing-1",
-                        title: "Digital Marketing",
-                        youtubeId: "abc123",
-                        description: "Marketing strategies",
-                        duration: "45:00",
-                        completed: false
-                    }
-                ]
-            }
-        ]
-    }
+                  {
+                    id: "css-1",
+                    title: "CSS Tutorial",
+                    youtubeId: "1Rs2ND1ryYc",
+                    description: "Complete CSS guide",
+                    duration: "6:18:38",
+                    completed: false,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "design",
+    name: "Design",
+    children: [
+      {
+        id: "ui-design",
+        name: "UI Design",
+        videos: [
+          {
+            id: "ui-1",
+            title: "UI Design Principles",
+            youtubeId: "9C8pxA7AecA",
+            description: "Learn UI design basics",
+            duration: "25:43",
+            completed: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "business",
+    name: "Business",
+    locked: false,
+    unlockMessage: "Enter business content code",
+    children: [
+      {
+        id: "marketing",
+        name: "Marketing",
+        videos: [
+          {
+            id: "marketing-1",
+            title: "Digital Marketing",
+            youtubeId: "abc123",
+            description: "Marketing strategies",
+            duration: "45:00",
+            completed: false,
+          },
+        ],
+      },
+    ],
+  },
 ];
